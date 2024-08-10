8c1f2a95e9abc5d25d75fef941327d4d7d6fe422
kevzcr <88956040+kevzcr@users.noreply.github.com>
2024-08-10 12:38:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '2b152099-977d-4b23-9e00-a7f9108c65ea';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
